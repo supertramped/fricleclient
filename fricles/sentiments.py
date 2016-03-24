ef6581a5abaaaf60766ac56db53ead515514dcdc
Chetan Ganjihal <chetan.ganjihal@gmail.com>
2016-03-24 14:28:48 +1100

    Update sentiments.py

--- fricles/sentiments.py
+++ fricles/sentiments.py
@@ -4,9 +4,9 @@
             pip install fricles
 
         >> How to use ?
-            from fricles import sentiments
+        from fricles import sentiments
         sent_op = sentiments().get_sentiments("api_key", "text")
-@author: Chetan G
+@author: Chetan Ganjihal
 @contact: chetan.ganjihal@gmail.com
 @since: Created on 2016-02-04
 @copyright: Copyright (C) 2016 Chetan Team. All rights reserved.
@@ -21,12 +21,12 @@ import requests
 class sentiments:
 
     url = "http://fricles.com:8080/fricles/api/v1/get_sentiments"
-    data = { "inputs" : {"api_key" :  "",  "text" :""} }
+    data = { "api_key" :  "",  "text" :""}
 
     def get_sentiments(self, api_key, text):
         inputData = copy.deepcopy(self.data.copy())
-        inputData["inputs"]["text"] = text
-        inputData["inputs"]["api_key"] = api_key
+        inputData["text"] = text
+        inputData["api_key"] = api_key
         try:
             r = requests.post(self.url, data=inputData)
             print r.status_code
